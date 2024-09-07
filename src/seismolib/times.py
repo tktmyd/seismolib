ff6b5c5692e88e1bea86306a6c2c4dd5c2869957
Takuto Maeda <6821825+tktmyd@users.noreply.github.com>
2024-09-07 20:53:52 +0900

    feat: added times.py

---
+++ src/seismolib/times.py
@@ -0,0 +1,190 @@
+import datetime as dt
+import numpy as np
+
+def time_range(start, stop, step=dt.timedelta(days=1)):
+
+    """datetime.date オブジェクトの start, stop の間を step 間隔で返すジェネレータ
+
+    yieldされるのは常にdatetime
+
+    Example
+    -------
+    for date in date_range(start, stop, step):
+        pass
+
+    Parameters
+    ----------
+    start : datetime.date or datetime.datetime
+        start day
+    stop : datetime.date or datetime.datetime
+        end day
+    step : datetime.timdedelta
+        step between start and stop
+
+    Yield
+    -----
+    datetime.datetime
+
+    Test
+    ----
+    >>> for tim in time_range(datetime.date(2020, 10, 5), datetime.date(2020, 10, 7)):
+    ...     print(tim)
+    2020-10-05 00:00:00
+    2020-10-06 00:00:00
+    2020-10-07 00:00:00
+
+    File
+    ----
+    seismolabpy/times.py
+    """
+
+    # 日付データの場合には 00:00:00 を追加してdatetime型にする
+    if type(start) == dt.date:
+        start = dt.datetime.combine(start, dt.time(0, 0, 0))
+        stop = dt.datetime.combine(stop, dt.time(0, 0, 0))
+
+    current = start
+
+    while current <= stop:
+        yield current
+
+        current += step
+
+
+def date2jul(date):
+
+    """Returns julian day (day of the year).
+
+    Parameters
+    -----------
+    date : datetime.date or datetime.datetime
+        A datetime object of which the julian day is calculated
+
+    Return
+    ------
+    julday: integer
+        Julian day of the year
+
+    Example
+    -------
+    >>> date2jul(datetime.date(2015, 12, 31))
+    365
+    """
+
+    return int(dt.datetime.strftime(date, "%j"))
+
+
+def jul2date(year, julday):
+
+    """Calculate datetime from year and julian day
+
+    Parameters
+    ----------
+    year, juday: integer
+
+    Return
+    ------
+    datetime.datetime
+
+    Example
+    >>> jul2date(2000, 366)
+    datetime.datetime(2000, 12, 31, 0, 0, 0)
+    """
+
+    return dt.datetime(year, 1, 1) + dt.timedelta(julday - 1)
+
+
+def is_leapyear(yr):
+
+    """True if the input yr is a leap year"""
+
+    return yr % 4 == 0 and yr % 100 != 0 or yr % 400 == 0
+
+
+def timegm(tim):
+
+    """Returns seconds from 1970-01-01 00:00:00 (UTC)
+
+    Parameters
+    ----------
+    tim: datetime.datetime
+        time to be converted
+
+    Returns:
+    float
+        time in seconds
+    """
+
+    if type(tim) == dt.date:
+        tim = dt.datetime.combine(tim, dt.time(0, 0, 0))
+
+    return tim.timestamp()
+
+
+def gmtime(t):
+
+    """Inverse of timegm
+
+    Parameters
+    ----------
+    t: float or int
+        seconds from 1970-01-01 00:00:00 (UTC)
+
+    Returns:
+    float
+        time in seconds
+    """
+    return dt.datetime.fromtimestamp(t)
+
+
+def date_time(year, month, day, hour, minute, second):
+
+    """日時情報からdatetime.datetimeオブジェクトを返す．secondが実数でもOK
+
+    Parameters
+    ----------
+    year   : int or str
+    month  : int or str
+    day    : int or str
+    hour   : int or str
+    minute : int or str
+    second : int or float or str
+        秒数は実数でも可．整数部とマイクロ秒部に分割してdatetimeに入力する．
+
+    Returns
+    -------
+    result: datetime.datetime object
+
+    Test
+    ----
+    >>> date_time(2021, 10, 1, 0, 0, 0.5)
+    datetime.datetime(2021, 10, 1, 0, 0, 0, 500000)
+
+    """
+
+    # float() や int() を噛ますのは，
+    # 入力が文字列であっても正常に動作させるため
+    fsec = float(second)
+    isec = int(fsec)
+    microsec = int(round((fsec - isec) * 1000)) * 1000
+    return dt.datetime(
+        int(year), int(month), int(day), int(hour), int(minute), isec, microsec
+    )
+
+
+def date(year, month, day):
+
+    return date_time(year, month, day, 0, 0, 0)
+
+
+def to_datetime(datetime64):
+
+    """convert datetime64 of numpy to datetime.datetime
+
+    Reference
+    ---------
+    https://gist.github.com/blaylockbk/1677b446bc741ee2db3e943ab7e4cabd
+    """
+
+    ts = (datetime64 - np.datetime64("1970-01-01T00:00:00")) / np.timedelta64(1, "s")
+    return dt.datetime.utcfromtimestamp(ts)
